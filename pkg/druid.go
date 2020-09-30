519f57c41ff4b71fcecf3cb2225711113ebe54a6
Jean-Baptiste Guerraz <861556+jbguerraz@users.noreply.github.com>
2020-09-30 05:07:48 +0200

    implemented instance management + refactor healthcheck

--- pkg/druid.go
+++ pkg/druid.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/bitly/go-simplejson"
 	"github.com/grafadruid/go-druid"
+	druidquery "github.com/grafadruid/go-druid/query"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
@@ -16,138 +16,80 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 )
 
-func newDatasource() datasource.ServeOpts {
-	// creates a instance manager for your plugin. The function passed
-	// into `NewInstanceManger` is called when the instance is created
-	// for the first time or when a datasource configuration changed.
-	im := datasource.NewInstanceManager(newDataSourceInstance)
-	ds := &DruidDatasource{
-		im: im,
+type druidQuery struct {
+	Builder  map[string]interface{} `json:"builder"`
+	Settings map[string]interface{} `json:"settings"`
+}
+
+func newDataSourceInstance(settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	log.DefaultLogger.Info("DRUID DS", "SETTINGS", settings)
+
+	data, err := simplejson.NewJson(settings.JSONData)
+	if err != nil {
+		return &druidInstanceSettings{}, err
+	}
+	secureData := settings.DecryptedSecureJSONData
+
+	var druidOpts []druid.ClientOption
+	if retryMax := data.Get("connection.retryableRetryMax").MustInt(-1); retryMax != -1 {
+		druidOpts = append(druidOpts, druid.WithRetryMax(retryMax))
+	}
+	if retryWaitMin := data.Get("connection.retryableRetryWaitMin").MustInt(-1); retryWaitMin != -1 {
+		druidOpts = append(druidOpts, druid.WithRetryWaitMin(time.Duration(retryWaitMin)*time.Millisecond))
+	}
+	if retryWaitMax := data.Get("connection.retryableRetryWaitMax").MustInt(-1); retryWaitMax != -1 {
+		druidOpts = append(druidOpts, druid.WithRetryWaitMax(time.Duration(retryWaitMax)*time.Millisecond))
+	}
+	if basicAuth := data.Get("connection.basicAuth").MustBool(); basicAuth {
+		druidOpts = append(druidOpts, druid.WithBasicAuth(data.Get("connection.basicAuthUser").MustString(), secureData["connection.basicAuthPassword"]))
 	}
 
+	c, err := druid.NewClient(data.Get("connection.url").MustString(), druidOpts...)
+	if err != nil {
+		return &druidInstanceSettings{}, err
+	}
+
+	return &druidInstanceSettings{
+		client: c,
+	}, nil
+}
+
+type druidInstanceSettings struct {
+	client       *druid.Client
+	queryContext map[string]interface{}
+}
+
+func (s *druidInstanceSettings) Dispose() {
+	s.client.Close()
+}
+
+func newDatasource() datasource.ServeOpts {
+	ds := &druidDatasource{
+		im: datasource.NewInstanceManager(newDataSourceInstance),
+	}
 	return datasource.ServeOpts{
 		QueryDataHandler:   ds,
 		CheckHealthHandler: ds,
 	}
 }
 
-type DruidDatasource struct {
-	// The instance manager can help with lifecycle management
-	// of datasource instances in plugins. It's not a requirements
-	// but a best practice that we recommend that you follow.
+type druidDatasource struct {
 	im instancemgmt.InstanceManager
 }
 
-// QueryData handles multiple queries and returns multiple responses.
-// req contains the queries []DataQuery (where each query contains RefID as a unique identifer).
-// The QueryDataResponse contains a map of RefID to the response for each query, and each response
-// contains Frames ([]*Frame).
-func (dd *DruidDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	log.DefaultLogger.Info("QueryData", "request", req)
-
-	// create response struct
-	response := backend.NewQueryDataResponse()
-
-	// loop over queries and execute them individually.
-	for _, q := range req.Queries {
-		res := dd.query(ctx, q)
-
-		// save the response in a hashmap
-		// based on with RefID as identifier
-		response.Responses[q.RefID] = res
-	}
-
-	return response, nil
-}
-
-type queryModel struct {
-	Format string `json:"format"`
-}
-
-func (dd *DruidDatasource) query(ctx context.Context, query backend.DataQuery) backend.DataResponse {
-	log.DefaultLogger.Info("QUERY SHOULD FIRE!", "QUERY", query)
-	log.DefaultLogger.Info("QUERY SHOULD FIRE!", "CTX", ctx)
-
-	// Unmarshal the json into our queryModel
-	var qm queryModel
-
-	response := backend.DataResponse{}
-
-	response.Error = json.Unmarshal(query.JSON, &qm)
-	if response.Error != nil {
-		return response
-	}
-
-	// Log a warning if `Format` is empty.
-	if qm.Format == "" {
-		log.DefaultLogger.Warn("format is empty. defaulting to time series")
-	}
-
-	// create data frame response
-	frame := data.NewFrame("response")
-
-	// add the time dimension
-	frame.Fields = append(frame.Fields,
-		data.NewField("time", nil, []time.Time{query.TimeRange.From, query.TimeRange.To}),
-	)
-
-	// add values
-	frame.Fields = append(frame.Fields,
-		data.NewField("values", nil, []int64{10, 20}),
-	)
-
-	// add the frames to the response
-	response.Frames = append(response.Frames, frame)
-
-	return response
-}
-
-func (dd *DruidDatasource) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
-	//type Settings struct {
-	//ConnectionBasicAuth             bool   `json:"connection.basicAuth"`
-	//ConnectionBasicAuthUser         string `json:"connection.basicAuthUser"`
-	//ConnectionRetryableRetryMax     int    `json:"connection.retryableRetryMax"`
-	//ConnectionRetryableRetryWaitMax int    `json:"connection.retryableRetryWaitMax"`
-	//ConnectionRetryableRetryWaitMin int    `json:"connection.retryableRetryWaitMin"`
-	//ConnectionURL                   string `json:"connection.url"`
-	//QueryContextParameters          []struct {
-	//Name  string `json:"name"`
-	//Value string `json:"value"`
-	//} `json:"query.contextParameters"`
-	//}
-
+func (ds *druidDatasource) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
 	result := &backend.CheckHealthResult{
 		Status:  backend.HealthStatusError,
 		Message: "Can't connect to Druid",
 	}
-	settings, err := simplejson.NewJson(req.PluginContext.DataSourceInstanceSettings.JSONData)
-	if err != nil {
-		result.Message = "Can't access datasource instance settings"
-		return result, nil
-	}
-	secureSettings := req.PluginContext.DataSourceInstanceSettings.DecryptedSecureJSONData
 
-	var druidOpts []druid.ClientOption
-	if retryMax := settings.Get("connection.retryableRetryMax").MustInt(-1); retryMax != -1 {
-		druidOpts = append(druidOpts, druid.WithRetryMax(retryMax))
-	}
-	if retryWaitMin := settings.Get("connection.retryableRetryWaitMin").MustInt(-1); retryWaitMin != -1 {
-		druidOpts = append(druidOpts, druid.WithRetryWaitMin(time.Duration(retryWaitMin)*time.Millisecond))
-	}
-	if retryWaitMax := settings.Get("connection.retryableRetryWaitMax").MustInt(-1); retryWaitMax != -1 {
-		druidOpts = append(druidOpts, druid.WithRetryWaitMax(time.Duration(retryWaitMax)*time.Millisecond))
-	}
-	if basicAuth := settings.Get("connection.basicAuth").MustBool(); basicAuth {
-		druidOpts = append(druidOpts, druid.WithBasicAuth(settings.Get("connection.basicAuthUser").MustString(), secureSettings["connection.basicAuthPassword"]))
-	}
-
-	d, err := druid.NewClient(settings.Get("connection.url").MustString(), druidOpts...)
+	i, err := ds.im.Get(req.PluginContext)
 	if err != nil {
-		result.Message = "Can't initialize a Druid client"
+		result.Message = "Can't get Druid instance"
 		return result, nil
 	}
 
-	status, _, err := d.Common().Status()
+	status, _, err := i.(*druidInstanceSettings).client.Common().Status()
 	if err != nil {
 		result.Message = "Can't fetch Druid status"
 		return result, nil
@@ -158,18 +100,61 @@ func (dd *DruidDatasource) CheckHealth(ctx context.Context, req *backend.CheckHe
 	return result, nil
 }
 
-type instanceSettings struct {
-	httpClient *http.Client
+func (ds *druidDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	response := backend.NewQueryDataResponse()
+
+	s, err := ds.settings(req.PluginContext)
+	if err != nil {
+		return response, err
+	}
+
+	for _, q := range req.Queries {
+		res := ds.query(ctx, q, s)
+		response.Responses[q.RefID] = res
+	}
+
+	return response, nil
 }
 
-func newDataSourceInstance(settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
-	log.DefaultLogger.Info("newDataSourceInstance", "setting", settings)
-	return &instanceSettings{
-		httpClient: &http.Client{},
-	}, nil
+func (ds *druidDatasource) settings(ctx backend.PluginContext) (*druidInstanceSettings, error) {
+	s, err := ds.im.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return s.(*druidInstanceSettings), nil
 }
 
-func (s *instanceSettings) Dispose() {
-	// Called before creating a new instance to allow plugin authors
-	// to cleanup.
+func (ds *druidDatasource) query(ctx context.Context, qry backend.DataQuery, s *druidInstanceSettings) backend.DataResponse {
+	// maybe implement a short life cache (druidInstanceSettings.cache ?) and early return then ?
+	response := backend.DataResponse{}
+
+	q, err := ds.prepareQuery(qry.JSON, s)
+	if err != nil {
+		response.Error = err
+		return response
+	}
+	log.DefaultLogger.Info("DRUID QUERY", "READY TO GO", q)
+
+	// execute the query and probably, based on ? queryType ? adapt the results to grafana kind of dataframe (wide, long)
+	frame := data.NewFrame("response")
+	frame.Fields = append(frame.Fields,
+		data.NewField("time", nil, []time.Time{qry.TimeRange.From, qry.TimeRange.To}),
+	)
+	frame.Fields = append(frame.Fields,
+		data.NewField("values", nil, []int64{10, 20}),
+	)
+	response.Frames = append(response.Frames, frame)
+
+	return response
+}
+
+func (ds *druidDatasource) prepareQuery(druidJSONQuery json.RawMessage, s *druidInstanceSettings) (druidquery.Query, error) {
+	var q druidQuery
+	err := json.Unmarshal(druidJSONQuery, &q)
+	if err != nil {
+		log.DefaultLogger.Info("DRUID QUERY", "FAILED", err)
+		return nil, err
+	}
+	//merge q.Settings.contextParameters with druidInstanceSettings.contextParameters, tranform parameters into query context and add it as druidquery.Query.context
+	return nil, nil
 }
