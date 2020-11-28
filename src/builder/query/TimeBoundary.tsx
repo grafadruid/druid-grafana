3436be87f9053afefe5a2ba751bbf20801289bb1
Jean-Baptiste Guerraz <861556+jbguerraz@users.noreply.github.com>
2020-11-29 00:19:08 +0100

    Timeboundary support

--- src/builder/query/TimeBoundary.tsx
+++ src/builder/query/TimeBoundary.tsx
@@ -129,7 +129,11 @@ export class TimeBoundary extends PureComponent<QueryBuilderProps, State> {
   onSelectionChange = (component: string, option: SelectableValue<string>) => {
     const { options, onOptionsChange } = this.props;
     const { builder } = options;
-    builder[component] = option.value;
+    let value = null;
+    if (option !== null) {
+      value = option.value;
+    }
+    builder[component] = value;
     onOptionsChange({ ...options, builder });
   };
 
