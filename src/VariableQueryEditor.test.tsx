6a58bae948fa09654d699f106b48c85c2f9a7c7b
Rakesh S <4614260+raakasf@users.noreply.github.com>
2021-03-26 16:47:46 -0400

    Updated tests

--- src/VariableQueryEditor.test.tsx
+++ src/VariableQueryEditor.test.tsx
@@ -3,10 +3,20 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 
 import { VariableQueryEditor } from './VariableQueryEditor';
+import ErrorBoundary from './ErrorBoundary';
 
 describe('VariableQueryEditor', function () {
   const fakeProps = {
-    query: { builder: {}, settings: {}, expr: '', refId: '' },
+    query: {
+      builder: {
+        query: 'SELECT DISTINCT countryName FROM wikipedia WHERE countryName IS NOT NULL',
+        queryType: 'sql',
+      },
+      settings: { contextParameters: [{ name: 'AA', value: 'BB' }], format: 'wide' },
+      expr:
+        '{"builder":{"query":"SELECT DISTINCT countryName FROM wikipedia WHERE countryName IS NOT NULL","queryType":"sql"},"settings":{"contextParameters":[{"name":"AA","value":"BB"}],"format":"wide"}}',
+      refId: '',
+    },
     onChange() {},
     css: '',
   };
@@ -15,6 +25,16 @@ describe('VariableQueryEditor', function () {
       const wrapper = shallow(<VariableQueryEditor {...fakeProps} />);
       console.log(wrapper.debug());
       // expect(wrapper.find(FieldSet)).toHaveLength(1);
+      // expect(wrapper.find(Field)).toHaveLength(1);
+      // expect(wrapper.find(Switch)).toHaveLength(1);
+      // expect(wrapper.find(Switch).prop('value')).toBe(false);
+    });
+  });
+  describe('when initialized', () => {
+    it('should render without basic authentication', function () {
+      const wrapper = shallow(<VariableQueryEditor {...fakeProps} />);
+      console.log(wrapper.debug());
+      expect(wrapper.find(ErrorBoundary)).toHaveLength(1);
       // expect(wrapper.find(Field)).toHaveLength(1);
       // expect(wrapper.find(Switch)).toHaveLength(1);
       // expect(wrapper.find(Switch).prop('value')).toBe(false);
