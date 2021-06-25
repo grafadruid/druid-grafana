e71d2f372d27de24f81363bb898e972f520998cb
G Gordon Worley III <ggworley@plaid.com>
2021-06-25 08:24:49 -0700

    Add a debounce to autosubmit. Set default of 250ms (#86)
    * Add a debounce to autosubmit. Set default of 250ms
    Co-authored-by: jbguerraz <861556+jbguerraz@users.noreply.github.com>

--- src/configuration/QuerySettings/DruidQueryDefaultSettings.tsx
+++ src/configuration/QuerySettings/DruidQueryDefaultSettings.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FieldSet } from '@grafana/ui';
 import { css, cx } from 'emotion';
-import { DruidQueryContextSettings, DruidQueryResponseSettings } from './';
+import { DruidQueryRequestSettings, DruidQueryResponseSettings } from './';
 import { QuerySettingsProps } from './types';
 
 export const DruidQueryDefaultSettings = (props: QuerySettingsProps) => {
   return (
     <>
-      <FieldSet label="Query" className={cx(styles.fieldset)}>
-        <DruidQueryContextSettings {...props} />
+      <FieldSet label="Request" className={cx(styles.fieldset)}>
+        <DruidQueryRequestSettings {...props} />
       </FieldSet>
       <FieldSet label="Response" className={cx(styles.fieldset)}>
         <DruidQueryResponseSettings {...props} />
