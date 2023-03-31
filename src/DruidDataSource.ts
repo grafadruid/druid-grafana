70f13b562f472491a06f240f994fe2da3cf30bc1
Oliver Linnarsson <oliver@lolo.company>
2023-03-31 13:42:15 +0200

    feat: Implement partial adhoc filter support

--- src/DruidDataSource.ts
+++ src/DruidDataSource.ts
@@ -1,21 +1,51 @@
 import { DataSourceInstanceSettings, MetricFindValue, ScopedVars } from '@grafana/data';
-import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
-import { DruidSettings, DruidQuery } from './types';
+import { DataSourceWithBackend, getTemplateSrv, logDebug } from '@grafana/runtime';
+import { DruidSettings, DruidQuery, AdhocVariableModel } from './types';
 
 const druidVariableRegex = /\"\[\[(\w+)(?::druid:(\w+))?\]\]\"|\"\${(\w+)(?::druid:(\w+))?}\"/g;
 
 export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSettings> {
   settingsData: DruidSettings;
+
   constructor(instanceSettings: DataSourceInstanceSettings<DruidSettings>) {
     super(instanceSettings);
     this.settingsData = instanceSettings.jsonData;
   }
+
   filterQuery(query: DruidQuery) {
     return !query.hide;
   }
+
+  _applyAdhocTemplateVariables(templatedQuery: DruidQuery, scopedVars?: ScopedVars) {
+    const adhocVariables = getTemplateSrv().getVariables()
+      .filter((it): it is AdhocVariableModel => it.type === 'adhoc');
+    if (adhocVariables.length === 0) return templatedQuery;
+
+    switch (templatedQuery.builder.queryType) {
+      case 'sql':
+        return ({
+          ...templatedQuery,
+          builder: {
+            ...templatedQuery.builder,
+            query: `${templatedQuery.builder.query}${
+              adhocVariables
+                .reduce((acc, it) => [
+                  ...acc,
+                  ...it.filters.map(it => `${it.key} ${it.operator} '${it.value}'`)
+                ], [/* adds leading " AND "*/''] as string[])
+                .join(' AND ')
+            }`,
+          }
+        });
+      default:
+        logDebug(`Adhoc variables aren't supported for queries of type "${templatedQuery.builder.queryType}"`);
+        return templatedQuery;
+    }
+  }
+
   applyTemplateVariables(templatedQuery: DruidQuery, scopedVars?: ScopedVars) {
     const templateSrv = getTemplateSrv();
-    let template = JSON.stringify({ ...templatedQuery, expr: undefined }).replace(
+    const template = JSON.stringify({ ...this._applyAdhocTemplateVariables(templatedQuery, scopedVars), expr: undefined }).replace(
       druidVariableRegex,
       (match, variable1, format1, variable2, format2) => {
         if (format1 || format2 === 'json') {
@@ -26,9 +56,36 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
     );
     return { ...JSON.parse(templateSrv.replace(template, scopedVars)), expr: templatedQuery.expr };
   }
+
   async metricFindQuery(query: DruidQuery, options?: any): Promise<MetricFindValue[]> {
     return this.postResource('query-variable', this.applyTemplateVariables(query)).then((response) => {
       return response;
     });
+  }
+
+  /**
+   * Provides autocompletion for adhoc filter keys
+   */
+  async getTagKeys(): Promise<MetricFindValue[]> {
+    return [
+      {
+        text: 'channel',
+      }
+    ]
+  }
+
+  /**
+   * Provides autocompletion for adhoc filter values given the chosen key
+   */
+  async getTagValues(options: { key: string }): Promise<MetricFindValue[]> {
+    console.log('getTagValues', options);
+    return [
+      {
+        text: '#en.wikipedia'
+      },
+      {
+        text: '#pl.wikipedia'
+      }
+    ]
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { DataQuery, DataSourceJsonData } from '@grafana/data';
+import { DataQuery, DataSourceJsonData, MetricFindValue, VariableModel } from '@grafana/data';
 import { QuerySettings } from './configuration/QuerySettings/types';
 import { ConnectionSettings } from './configuration/ConnectionSettings/types';
 
@@ -15,3 +15,28 @@ export interface DruidSettings extends DataSourceJsonData {
 }
 
 export interface DruidSecureSettings {}
+
+export interface AdhocFilter {
+  key: string;
+  operator: '=' | '!=' | '<' | '>' | '=~' | '!~';
+  value: MetricFindValue['value'],
+  condition: unknown;
+}
+
+export interface AdhocVariableModel extends VariableModel {
+  type: "adhoc";
+  id: string;
+  datasource: {
+    type: string;
+    uid: string;
+  };
+  filters: AdhocFilter[],
+  hide: number;
+  skipUrlSync: boolean;
+  rootStateKey: string;
+  global: boolean;
+  index: number;
+  state: string;
+  error: unknown;
+  description: unknown;
+}
