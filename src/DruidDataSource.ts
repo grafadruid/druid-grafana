0e77936d511dc19b344f0702abfd5c551980a5f1
Oliver Linnarsson <oliver@lolo.company>
2023-04-06 13:19:46 +0200

    feat: Improve reliability of adhoc variables in sql queries

--- src/DruidDataSource.ts
+++ src/DruidDataSource.ts
@@ -1,6 +1,7 @@
 import { DataSourceInstanceSettings, MetricFindValue, ScopedVars } from '@grafana/data';
 import { DataSourceWithBackend, getTemplateSrv, logWarning } from '@grafana/runtime';
-import { DruidSettings, DruidQuery, AdhocVariableModel, AdhocFilter } from './types';
+import { SQL, type SqlExpression } from './util/SQL';
+import type { DruidSettings, DruidQuery, AdhocVariableModel, AdhocFilter } from './types';
 
 const druidVariableRegex = /\"\[\[(\w+)(?::druid:(\w+))?\]\]\"|\"\${(\w+)(?::druid:(\w+))?}\"/g;
 
@@ -16,12 +17,186 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
     return !query.hide;
   }
 
-  _applyAdhocTemplateVariables(templatedQuery: DruidQuery, scopedVars?: ScopedVars) {
+  _applyAdhocTemplateVariablesToSQL(templatedQuery: DruidQuery, adhocFilters: AdhocFilter[]) {
+    const query = SQL.parseSelectQuery(templatedQuery.builder.query);
+
+    const initialWhere = (() => {
+      if (query.where) {
+        return query.where;
+      }
+      const firstAdhoc = adhocFilters.pop();
+      return {
+        type: 'binary_expr',
+        operator: firstAdhoc?.operator,
+        left: {
+            type: 'column_ref',
+            table: null,
+            column: firstAdhoc?.key
+        },
+        right: {
+            type: 'single_quote_string',
+            value: firstAdhoc?.value
+        }
+      } as SqlExpression;
+    })();
+
+    query.where = adhocFilters.reduce<SqlExpression>((acc, filter) => {
+      const expression = ((): SqlExpression => {
+        if ((filter.operator === '<' || filter.operator === '>')) {
+          return {
+            type: 'binary_expr',
+            operator: filter.operator,
+            left: {
+              type: 'column_ref',
+              table: null,
+              column: filter.key,
+            },
+            right: typeof filter.value === 'number' ? {
+              type: 'number',
+              value: filter.value
+            } : {
+              type: 'single_quote_string',
+              value: String(filter.value),
+            },
+          };
+        } else if (filter.operator === '!~') {
+          return {
+            type: 'unary_expr',
+            operator: 'NOT',
+            expr: {
+              type: 'binary_expr',
+              operator: 'LIKE',
+              left: {
+                type: 'column_ref',
+                table: null,
+                column: filter.key,
+              },
+              right: {
+                type: 'single_quote_string',
+                value: String(filter.value),
+              },
+            },
+          };
+        } else if (filter.operator === '=~') {
+          return {
+            type: 'binary_expr',
+            operator: 'LIKE',
+            left: {
+              type: 'column_ref',
+              table: null,
+              column: filter.key,
+            },
+            right: {
+              type: 'single_quote_string',
+              value: String(filter.value),
+            },
+          };
+        }
+        return {
+          type: 'binary_expr',
+          operator: filter.operator,
+          left: {
+            type: 'column_ref',
+            table: null,
+            column: filter.key,
+          },
+          right: {
+            type: 'single_quote_string',
+            value: String(filter.value),
+          },
+        };
+      })() ;
+
+      return {
+        type: 'binary_expr',
+        operator: 'AND',
+        left: expression,
+        right: acc,
+      };
+    }, initialWhere);
+
+    return {
+      ...templatedQuery,
+      builder: {
+        ...templatedQuery.builder,
+        query: SQL.stringify(query),
+      },
+    };
+  }
+
+  _applyAdhocTemplateVariablesToNativeQuery(templatedQuery: DruidQuery, adhocFilters: AdhocFilter[]) {
+    return {
+      ...templatedQuery,
+      builder: {
+        ...templatedQuery.builder,
+        filter: {
+          type: 'and',
+          fields: [
+            templatedQuery.builder.filter,
+            ...adhocFilters.map((it) => {
+              switch (it.operator) {
+                case '=':
+                  return {
+                    type: 'selector',
+                    dimension: it.key,
+                    value: it.value,
+                  };
+                case '!=':
+                  return {
+                    type: 'not',
+                    field: {
+                      type: 'selector',
+                      dimension: it.key,
+                      value: it.value,
+                    },
+                  };
+                case '<':
+                  return {
+                    type: 'bound',
+                    ordering: 'numeric',
+                    dimension: it.key,
+                    upper: it.value,
+                  };
+                case '>':
+                  return {
+                    type: 'bound',
+                    ordering: 'numeric',
+                    dimension: it.key,
+                    lower: it.value,
+                  };
+                case '=~':
+                  return {
+                    type: 'like',
+                    dimension: it.key,
+                    pattern: it.value,
+                  };
+                case '!~':
+                  return {
+                    type: 'not',
+                    field: {
+                      type: 'like',
+                      dimension: it.key,
+                      value: it.value,
+                    },
+                  };
+                default:
+                  logWarning(`Skipping unexpected filter operator: ${it.operator}`);
+                  return null;
+              }
+            }),
+          ].filter(it => it !== null),
+        },
+      },
+    };
+  }
+
+  _applyAdhocTemplateVariables(templatedQuery: DruidQuery) {
     const adhocFilters = getTemplateSrv()
       .getVariables()
       .filter((it): it is AdhocVariableModel => it.type === 'adhoc')
       .filter((it) => it.datasource && it.datasource.uid === this.uid)
-      .reduce((acc, it) => [...acc, ...it.filters], [] as AdhocFilter[]);
+      .reduce((acc, it) => [...acc, ...it.filters], [] as AdhocFilter[])
+      .filter((it) => it.value !== undefined);
 
     if (adhocFilters.length === 0) {
       return templatedQuery;
@@ -29,110 +204,22 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
 
     switch (templatedQuery.builder.queryType) {
       case 'sql':
-        return {
-          ...templatedQuery,
-          builder: {
-            ...templatedQuery.builder,
-            query: `${templatedQuery.builder.query} ${templatedQuery.builder.query.indexOf('WHERE') >= 0 ? 'AND' : 'WHERE'} ${adhocFilters
-              .map((it) => {
-                switch (it.operator) {
-                  case '=':
-                  case '!=':
-                  case '<':
-                  case '>':
-                    return `${it.key} ${it.operator} '${it.value}'`;
-
-                  case '=~':
-                    return `${it.key} LIKE '${it.value}'`;
-                  case '!~':
-                    return `${it.key} NOT LIKE '${it.value}'`;
-                  default:
-                    logWarning(`Skipping unexpected filter operator: ${it.operator}`);
-                    return null;
-                }
-              })
-              .filter(it => it !== null)
-              .join(' AND ')}`,
-          },
-        };
+        return this._applyAdhocTemplateVariablesToSQL(templatedQuery, adhocFilters);
       case 'timeseries':
       case 'topN':
       case 'groupBy':
       case 'scan':
       case 'search':
       case 'timeBoundary':
-        return {
-          ...templatedQuery,
-          builder: {
-            ...templatedQuery.builder,
-            filter: {
-              type: 'and',
-              fields: [
-                templatedQuery.builder.filter,
-                ...adhocFilters.map((it) => {
-                  switch (it.operator) {
-                    case '=':
-                      return {
-                        type: 'selector',
-                        dimension: it.key,
-                        value: it.value,
-                      };
-                    case '!=':
-                      return {
-                        type: 'not',
-                        field: {
-                          type: 'selector',
-                          dimension: it.key,
-                          value: it.value,
-                        },
-                      };
-                    case '<':
-                      return {
-                        type: 'bound',
-                        ordering: 'numeric',
-                        dimension: it.key,
-                        upper: it.value,
-                      };
-                    case '>':
-                      return {
-                        type: 'bound',
-                        ordering: 'numeric',
-                        dimension: it.key,
-                        lower: it.value,
-                      };
-                    case '=~':
-                      return {
-                        type: 'like',
-                        dimension: it.key,
-                        pattern: it.value,
-                      };
-                    case '!~':
-                      return {
-                        type: 'not',
-                        field: {
-                          type: 'like',
-                          dimension: it.key,
-                          value: it.value,
-                        },
-                      };
-                    default:
-                      logWarning(`Skipping unexpected filter operator: ${it.operator}`);
-                      return null;
-                  }
-                }),
-              ].filter(it => it !== null),
-            },
-          },
-        };
+        return this._applyAdhocTemplateVariablesToNativeQuery(templatedQuery, adhocFilters);
       default:
         return templatedQuery;
     }
   }
 
   applyTemplateVariables(templatedQuery: DruidQuery, scopedVars?: ScopedVars) {
-    const templateSrv = getTemplateSrv();
     const template = JSON.stringify({
-      ...this._applyAdhocTemplateVariables(templatedQuery, scopedVars),
+      ...templatedQuery,
       expr: undefined,
     }).replace(druidVariableRegex, (match, variable1, format1, variable2, format2) => {
       if (format1 || format2 === 'json') {
@@ -140,7 +227,10 @@ export class DruidDataSource extends DataSourceWithBackend<DruidQuery, DruidSett
       }
       return match;
     });
-    return { ...JSON.parse(templateSrv.replace(template, scopedVars)), expr: templatedQuery.expr };
+    return this._applyAdhocTemplateVariables({
+      ...JSON.parse(getTemplateSrv().replace(template, scopedVars)),
+      expr: templatedQuery.expr,
+    });
   }
 
   async metricFindQuery(query: DruidQuery, options?: any): Promise<MetricFindValue[]> {
---
+++ src/util/SQL.ts
@@ -0,0 +1,73 @@
+import * as NodeSqlParser from 'node-sql-parser';
+
+export const SQL = (() => {
+  const parser = new NodeSqlParser.Parser();
+  const options = {
+    database: 'postgresql',
+  };
+
+  const api = {
+    parse: (queryString: string) => parser.astify(queryString, options),
+    parseSelectQuery: (queryString: string) => {
+      const query = api.parse(queryString);
+      if (Array.isArray(query)) {
+        throw Error('Unexpected mutiple sql query');
+      }
+      if (query.type !== 'select') {
+        throw Error('Unexpected non-select query type');
+      }
+      return query as SqlSelectQuery;
+    },
+    stringify: (query: NodeSqlParser.AST) => parser.sqlify(query, options)
+  };
+  return api;
+})();
+
+interface SqlColumnRef {
+  type: 'column_ref';
+  table: string | null;
+  column: string;
+}
+
+interface SqlStringValue {
+  type: 'single_quote_string';
+  value: string;
+}
+
+interface SqlNullValue {
+  type: 'null';
+  value: null;
+}
+
+interface SqlNumberValue {
+  type: 'number';
+  value: number;
+}
+
+interface SqlUnaryExpression {
+  type: 'unary_expr',
+  operator: string,
+  expr: SqlExpression;
+}
+
+type SqlBinaryExpression =
+  | {
+    type: 'binary_expr';
+    operator: string;
+    left: SqlExpression;
+    right: SqlExpression;
+  }
+  | {
+    type: 'binary_expr';
+    operator: string;
+    left: SqlColumnRef;
+    right: SqlValue;
+  };
+
+export type SqlValue = SqlStringValue | SqlNullValue | SqlNumberValue;
+
+export type SqlExpression = SqlBinaryExpression | SqlUnaryExpression;
+
+export interface SqlSelectQuery extends NodeSqlParser.Select {
+  where: SqlExpression | null;
+}
