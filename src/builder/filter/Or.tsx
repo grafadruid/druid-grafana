42e80a84b6c4dd083caba44203c93b8217e028e5
Sudipta Kar <sudipta@poshmark.com>
2026-02-12 16:01:52 +0530

    sync and with or

--- src/builder/filter/Or.tsx
+++ src/builder/filter/Or.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { QueryBuilderProps } from '../types';
-import { useScopedQueryBuilderFieldProps, Multiple, Row } from '../abstract';
+import { useScopedQueryBuilderFieldProps, Multiple } from '../abstract';
 import { Filter } from './';
 
 export const Or = (props: QueryBuilderProps) => {
   const scopedProps = useScopedQueryBuilderFieldProps(props, Or);
   return (
-    <>
-      <Row>
         <Multiple
           {...scopedProps('fields')}
-          label="Filters"
-          description="The filters"
+          label="Fields"
+          description="The filters Fields"
           component={Filter}
           componentExtraProps={{}}
         />
-      </Row>
-    </>
   );
 };
 Or.type = 'or';
